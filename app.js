3db4c2947d41385eef29d33020277bb05a416392
Eric Lin <tinyjingle@gmail.com>
2015-01-27 20:45:24 -0800

    Added libs

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-var path = require("path");
-var express = require('express');
-var app = express();
-var scoreBoard = [{
-    name: 'Eric',
-    score: '50'
-},{
-    name: 'Erac',
-    score: '40'
-},{
-    name: 'Erika',
-    score: '60'
-}];
+var path = require("path"),
+    express = require('express'),
+    app = express();
 
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
 app.use(express.static(path.resolve(path.dirname(require.main.filename), 'public')));
 
 var server = app.listen(3000, function() {
